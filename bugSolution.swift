9146d8d456c723b9d4f6f96664ea6aeacfef22b0
BugHunter-X <askzenai@gmail.com>
2025-01-23 21:10:05 +0100

    Add bugSolution.swift file

---
+++ bugSolution.swift
@@ -0,0 +1,14 @@
+let array = [1, 2, 3]
+let index = 4
+if index < array.count {
+    print(array[index])
+} else {
+    print("Index out of bounds")
+}
+
+//Alternatively, using optional binding:
+if let element = array.indices.contains(index) ? array[index] : nil {
+    print(element)
+} else {
+    print("Index out of bounds")
+}
